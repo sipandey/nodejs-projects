b6c86eac5637f4860d72b7b7bfe0b16dbce5bbab
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-04-20 17:56:18 +0530

    adding web-server

---
+++ web-server/src/app.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'forecast',
+        location: 'location'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
