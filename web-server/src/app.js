6feea6cc7bd9031644a104fa754262ccbf30944f
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-04-20 18:18:29 +0530

    exploring handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Siddharth'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Us Page',
+        name: 'Siddharth'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is the help page.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
