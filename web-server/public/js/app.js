83bead09165156774ee2b6e387e7b39092be29a3
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-04-21 11:41:15 +0530

    search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,24 @@
 console.log('Client side js file is loaded')
 
-fetch('http://localhost:3000/weather?address=Gurgaon').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        }
-        else {
-            console.log(data.location, data.forecast)
-        }
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    // prevents reload of the page when submitting
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            }
+            else {
+                console.log(data.location, data.forecast)
+            }
+        })
     })
 })
