4069b253026d23ccd4d101a931aab431711af7ff
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-04-21 12:11:16 +0530

    wiring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,6 +4,8 @@ console.log('Client side js file is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     // prevents reload of the page when submitting
@@ -11,13 +13,17 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             }
             else {
-                console.log(data.location, data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
