b251ade120e6ddaad4c001b1b070b79282823ac8
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-04-21 11:29:46 +0530

    http requests with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side js file is loaded')
+
+fetch('http://localhost:3000/weather?address=Gurgaon').then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error)
+        }
+        else {
+            console.log(data.location, data.forecast)
+        }
+    })
+})
