bb8cde703798b928c11b4bb82ab25b7fccf49854
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-04 23:38:12 +0530

    deleting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -51,12 +51,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     console.log(error)
     // })
 
-    db.collection('tasks').updateMany({
-        completed: false
-    },{
-        $set: {
-            completed: true
-        }
+    // db.collection('tasks').updateMany({
+    //     completed: false
+    // },{
+    //     $set: {
+    //         completed: true
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    db.collection('users').deleteMany({
+        age: 28
     }).then((result) => {
         console.log(result)
     }).catch((error) => {
