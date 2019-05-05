12668419bc14d398227a336482eebb6c824f21a1
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-05 22:17:10 +0530

    async await examples

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5cceeddd825219381c52a2da').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5cceeddd825219381c52a2da').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+
+deleteTaskAndCount('5ccdda0afdb2b82bd0c114e1').then((count) => {
+    console.log(count)
 }).catch((err) => {
     console.log(err)
-})
+});
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -3,12 +3,24 @@ const User = require('../src/models/user')
 
 //5ccdde4ff0d17a397cb3c775
 
-User.findByIdAndUpdate('5ccde02144b07d41600510e8', {
-    age: 1
-}).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
+// User.findByIdAndUpdate('5ccde02144b07d41600510e8', {
+//     age: 1
+// }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// });
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age: age})
+    const count = User.countDocuments({age: age})
+    return count
+}
+
+updateAgeAndCount('5ccde02144b07d41600510e8', 2).then((result) => {
     console.log(result)
 }).catch((err) => {
     console.log(err)
