51ee388365ea6d1b5d85c90b5a22db38a18c76da
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-05 20:20:42 +0530

    promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,13 +1,36 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([7,4,1])
-        reject('This is error!')
-    }, 2000)
-})
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a+b)
+        }, 2000)
+    })
+}
 
-doWorkPromise.then((result) => {
-    console.log(result)
-}).catch((error) => {
-    console.log(error)
-})
+// add(1,2).then((sum) => {
+//     console.log(sum)
+// }).catch((err) => {
+//     console.log(err)
+// });
+
+add(5,4).then((sum) => {
+    console.log(sum)
+    return add(sum, 5)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((err) => {
+    console.log(err)
+});
+
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve([7,4,1])
+//         reject('This is error!')
+//     }, 2000)
+// })
+
+// doWorkPromise.then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
 
