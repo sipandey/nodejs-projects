b86d78a8b0c06478e4f5e7590569d2176dd48fe5
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-05 23:40:54 +0530

    updating and deleting endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -31,7 +31,7 @@ app.get('/users', async (req, res) => {
         const users = await User.find({})
         res.send(users)
     } catch (error) {
-        res.status(500).send()
+        res.status(500).send(error)
     }
     // User.find({}).then((users) => {
     //     res.send(users);
@@ -49,7 +49,7 @@ app.get('/users/:id', async (req, res) => {
         }
         res.send(user)
     } catch (error) {
-        res.status(500).send()
+        res.status(500).send(error)
     }
 
     // const _id = req.params.id
@@ -62,6 +62,41 @@ app.get('/users/:id', async (req, res) => {
     // }).catch((err) => {
     //     res.status(500).send();
     // });
+});
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid Operations'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(error)        
+    }
 });
 
 app.post('/tasks', async (req, res) => {
@@ -117,6 +152,42 @@ app.get('/tasks/:id', async (req, res) => {
     // }).catch((err) => {
     //     res.status(500).send();
     // });
+});
+
+app.patch('/tasks/:id', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid Operation'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+    
 });
 
 app.listen(port, () => {
