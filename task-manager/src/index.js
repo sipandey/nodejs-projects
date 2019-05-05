86910860ac18e2908c3ec07e0329f4523ce9bfdc
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-05 20:11:13 +0530

    endpoint enhancements

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,6 +18,27 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch((err) => {
+        res.status(500).send();
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user);
+    }).catch((err) => {
+        res.status(500).send();
+    });
+});
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -26,6 +47,27 @@ app.post('/tasks', (req, res) => {
     }).catch((err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((err) => {
+        res.status(500).send();
+    });  
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch((err) => {
+        res.status(500).send();
+    });  
 });
 
 app.listen(port, () => {
