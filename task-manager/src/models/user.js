5da9df6a0540e3551c44dde39c6cbd6d4b6d15a5
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-08 11:51:20 +0530

    commit

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -41,6 +41,23 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email: email})
+
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password before savig=ng
 userSchema.pre('save', async function(next) {
     const user = this
 
