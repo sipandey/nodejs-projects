8d5c28af6c877178a48f5dfd21d27a73759de007
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-11 15:12:35 +0530

    hiding private info

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,14 +14,3 @@ app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
 
-const jwt = require('jsonwebtoken')
-
-const myFunction = async() => {
-    const token = jwt.sign({ _id: 'abc123'}, 'thisismycourse', { expiresIn: '7 days'})
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismycourse')
-    console.log(data)
-}
-
-myFunction()
