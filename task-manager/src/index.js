3ced76ce2b124b0a7cc551d33a21842994918546
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-11 16:09:37 +0530

    user-task relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,3 +14,17 @@ app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
 
+// const Task = require('./models/task')
+// const User = require('./models/user')
+
+// const main = async () => {
+//     // const task = await Task.findById('5cd69d8072a9ac4c6cccb10c')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
+
+//     const user = await User.findById('5cd69cc80ded4856b854d9c6')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
+
+// main()
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -70,31 +70,31 @@ router.get('/users/me', auth, async (req, res) => {
     // });
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (error) {
-        res.status(500).send(error)
-    }
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
+//     try {
+//         const user = await User.findById(_id)
+//         if(!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (error) {
+//         res.status(500).send(error)
+//     }
 
-    // const _id = req.params.id
+//     // const _id = req.params.id
 
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user);
-    // }).catch((err) => {
-    //     res.status(500).send();
-    // });
-});
+//     // User.findById(_id).then((user) => {
+//     //     if (!user) {
+//     //         return res.status(404).send()
+//     //     }
+//     //     res.send(user);
+//     // }).catch((err) => {
+//     //     res.status(500).send();
+//     // });
+// });
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => {
@@ -108,29 +108,27 @@ router.patch('/users/:id', async (req, res) => {
     try {
         // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
-        const user = await User.findById(req.params.id)
         updates.forEach((update) => {
-            user[update] = req.body[update]
+            req.user[update] = req.body[update]
         })
 
-        await user.save()
-
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        await req.user.save()
+        res.send(req.user)
     } catch (error) {
         res.status(400).send(error);
     }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        // const user = await User.findByIdAndDelete(req.user._id)
+        
+        // if(!user) {
+        //     return res.status(404).send()
+        // }
+
+        await req.user.remove()
+        res.send(req.user)
     } catch (error) {
         res.status(500).send(error)        
     }
